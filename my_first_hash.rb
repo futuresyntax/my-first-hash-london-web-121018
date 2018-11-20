639b349ef2616c81aa70f760a3fe8d94a8fd6ad4
FutureScript <fernandobaxter1@gmail.com>
2018-11-20 14:02:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,10 +12,10 @@ def retrieval
 end
 
 def adding
-  shipping_manifest ["muskets"] = 2;
-  shipping_manifest ["gun powder"] = 4
+  shipping_manifest ["muskets"] = "2"
+  shipping_manifest ["gun powder"] = "4"
 end
-end
+
 
 
 #
