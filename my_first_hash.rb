1fa1bfc83f0f07ddc4dc0137e883bb4553387533
FutureScript <fernandobaxter1@gmail.com>
2018-11-20 13:53:10 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,13 +5,13 @@ end
 
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-end  
+end
 
 def retrieval
   shipping_manifest["oil paintings"]
 end
 
-def adding 
+def adding
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
 end 
