16dd1e1809847c2f2a5a15419dd445bfe7d415b7
FutureScript <fernandobaxter1@gmail.com>
2018-11-20 13:53:01 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,21 +1,22 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash = {"apple pie" => "delicious", "brussel sprouts" => "nasty"}
-
-
 end
 
 
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information:
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-end
+end  
 
 def retrieval
   shipping_manifest["oil paintings"]
 end
+
+def adding 
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+end 
+
+
 #
 # def retrieval
 #   shipping_manifest = {
