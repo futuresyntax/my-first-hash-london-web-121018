de1eb0a4b29559ccbd9a3b81f9290acdbb4842a5
FutureScript <fernandobaxter1@gmail.com>
2018-11-20 13:43:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,18 +14,18 @@ def shipping_manifest
 
 
 end
-# 
+#
 # def retrieval
 #   shipping_manifest = {
 #                   "whale bone corsets" => 5,
 #                   "porcelain vases" => 2,
 #                   "oil paintings" => 3
 #                  }
-# 
+#
 #   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-# 
+#
 # end
-# 
+#
 # def adding
 #   shipping_manifest = {
 #                   "whale bone corsets" => 5,
@@ -33,13 +33,13 @@ end
 #                   "oil paintings" => 3
 #                  }
 #   # add 2 muskets to the shipping_manifest hash below
-# 
-# 
-# 
+#
+#
+#
 #   # add 4 gun powder to the shipping_manifest hash below
-# 
-# 
-# 
+#
+#
+#
 #   # return the shipping_manifest hash below
-# 
+#
 # end
